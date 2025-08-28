98a2b88b2daa40e2dce173cfc6b59922574bcc2b
Payton Hansen <paymeaton12@gmail.com>
2025-08-27 23:32:12 -0600

    steps 1-4 partially done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+bool isValid = false;
+Console.Write("Please enter the message: ");
+string message = Console.ReadLine();
+isValid = InputIsGood(message);
+
+while (isValid == false)
+{
+    Console.WriteLine("Sorry we only support lower case letters.");
+    Console.Write("Please enter your message again: ");
+    message = Console.ReadLine();
+    isValid = InputIsGood(message);
+}
+Console.Write("Please enter the key: ");
+string key = Console.ReadLine();
+isValid = InputIsGood(key);
+
+while (isValid == false)
+{
+    Console.WriteLine("Sorry we only support lower case letters.");
+    Console.Write("Please enter your message again: ");
+    key = Console.ReadLine();
+    isValid = InputIsGood(key);
+}
+
+static bool InputIsGood(string input)
+{
+    char[] inputChar = input.ToCharArray();
+    foreach (char letter in inputChar)
+    {
+        if (IsLowercaseLetter(letter));
+        else
+        {
+            return false;
+        }
+        
+    }
+    return true;
+}
+
+// Returns true if the given character is a lowercase letter and false otherwise
+static bool IsLowercaseLetter(char c)
+{
+    return false;
+}
+
+// static void TestIsLowercaseLetter() {
+//     Debug.Assert(TestIsLowercaseLetter('a'));
+//     Debug.Assert(TestIsLowercaseLetter('b'));
+//     Debug.Assert(TestIsLowercaseLetter('z'));
+//     Debug.Assert(!TestIsLowercaseLetter('A'));
+//     Debug.Assert(!TestIsLowercaseLetter('`'));
+//     Debug.Assert(!TestIsLowercaseLetter('{'));
+// }
