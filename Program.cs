646a6f1058191cfc8de9961f6265cf45030a49d2
Payton Hansen <paymeaton12@gmail.com>
2025-08-27 23:52:09 -0600

    5-13 partially completed minus tests

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Diagnostics;
+﻿//Payton Hansen
+//Lab 01
+
+using System.Data;
+using System.Diagnostics;
 
 bool isValid = false;
 Console.Write("Please enter the message: ");
@@ -24,17 +28,20 @@ while (isValid == false)
     isValid = InputIsGood(key);
 }
 
+string shiftedMessage = MessageShift(message, key);
+Console.WriteLine($"Here is the encrypted message:\n {shiftedMessage}");
+
 static bool InputIsGood(string input)
 {
     char[] inputChar = input.ToCharArray();
     foreach (char letter in inputChar)
     {
-        if (IsLowercaseLetter(letter));
+        if (IsLowercaseLetter(letter)) ;
         else
         {
             return false;
         }
-        
+
     }
     return true;
 }
@@ -42,7 +49,35 @@ static bool InputIsGood(string input)
 // Returns true if the given character is a lowercase letter and false otherwise
 static bool IsLowercaseLetter(char c)
 {
+    int charIntValue = Convert.ToInt32(c);
+    for (int i = 97; i <= 122; i++)
+    {
+        if (c == i)
+        {
+            return true;
+        }
+    }
     return false;
+}
+static string MessageShift(string message, string key)
+{
+    char[] messageLetters = message.ToCharArray();
+    char[] keyLetters = key.ToCharArray();
+    int count = 0;
+    foreach (char letter in messageLetters)
+    {
+        LetterShift(letter, keyLetters[count]);
+        count++;
+        if (count >= keyLetters.Length)
+        {
+            count = 0;
+        }
+    }
+
+}
+static char LetterShift(char message, char key)
+{
+    return message;
 }
 
 // static void TestIsLowercaseLetter() {
